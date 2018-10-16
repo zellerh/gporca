c425bba7957e6d3bff2e095c154469d5a7ed7fc9
Rahul Iyer <riyer@apache.org>
2018-10-16 11:15:12 -0700

    EagerAgg: Add new xform to push down group-by aggregates                                                                                                                                                                                              [0/976]
    Goal of this work is to add a transform that eagerly aggregates data if
    an inner join is present below the aggregate. This allows, potentially,
    reducing the number of tuples to join, hence making this plan cheaper
    than alternatives.
    
    Current restrictions include:
    1. Only aggregates using columns from one side of the inner join are supported
    2. This commit adds the feature for `min` and `max`. Future commits will
    add support for `sum`, `count` and `avg`.
    3. DQAs are not supported
    
    Example query:
    ```
    SELECT min(s1)
    FROM foo, bar
    WHERE foo.j1 = bar.j2
    AND   foo.s1 % 2 = 0
    GROUP BY foo.g1, bar.g2
    ```
    
    Plan without EagerAgg
    ```
                                                      QUERY PLAN
    --------------------------------------------------------------------------------------------------------------
     Gather Motion 3:1  (slice2; segments: 3)  (cost=0.00..43788.53 rows=562500 width=4)
       ->  Result  (cost=0.00..43780.14 rows=187500 width=4)
             ->  HashAggregate  (cost=0.00..43780.14 rows=187500 width=4)
                   Group Key: foo.g1, bar.g2
                   ->  Redistribute Motion 3:3  (slice1; segments: 3)  (cost=0.00..43734.55 rows=187500 width=12)
                         Hash Key: foo.g1, bar.g2
                         ->  Result  (cost=0.00..43727.51 rows=187500 width=12)
                               ->  HashAggregate  (cost=0.00..43727.51 rows=187500 width=12)
                                     Group Key: foo.g1, bar.g2
                                     ->  Hash Join  (cost=0.00..11032.52 rows=132874743 width=12)
                                           Hash Cond: (foo.j1 = bar.j2)
                                           ->  Table Scan on foo  (cost=0.00..2595.31 rows=13333572 width=12)
                                                 Filter: ((s1 % 2) = 0)
                                           ->  Hash  (cost=431.77..431.77 rows=33341 width=8)
                                                 ->  Table Scan on bar  (cost=0.00..431.77 rows=33341 width=8)
    ```
    
    Plan with EagerAgg
    ```
                                                       QUERY PLAN
    ----------------------------------------------------------------------------------------------------------------
     Gather Motion 3:1  (slice2; segments: 3)  (cost=0.00..12090.26 rows=562500 width=4)
       ->  Result  (cost=0.00..12081.88 rows=187500 width=4)
             ->  HashAggregate  (cost=0.00..12081.88 rows=187500 width=4)
                   Group Key: foo.g1, bar.g2
                   ->  Redistribute Motion 3:3  (slice1; segments: 3)  (cost=0.00..12036.29 rows=187500 width=12)
                         Hash Key: foo.g1, bar.g2
                         ->  Result  (cost=0.00..12029.25 rows=187500 width=12)
                               ->  HashAggregate  (cost=0.00..12029.25 rows=187500 width=12)
                                     Group Key: foo.g1, bar.g2
                                     ->  Hash Join  (cost=0.00..7414.04 rows=18754313 width=12)
                                           Hash Cond: (foo.j1 = bar.j2)
                                           ->  HashAggregate  (cost=0.00..5843.87 rows=1881938 width=12)
                                                 Group Key: foo.j1, foo.g1
                                                 ->  Table Scan on foo  (cost=0.00..2595.31 rows=13333572 width=12)
                                                       Filter: ((s1 % 2) = 0)
                                           ->  Hash  (cost=431.77..431.77 rows=33341 width=8)
                                                 ->  Table Scan on bar  (cost=0.00..431.77 rows=33341 width=8)
    ```
    
    Co-authored-by: Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
    Co-authored-by: Venkatesh Raghavan <vraghavan@pivotal.io>
    Co-authored-by: Sambitesh Dash <sdash@pivotal.io>

---
+++ libgpopt/include/gpopt/xforms/CXformEagerAgg.h
@@ -0,0 +1,107 @@
+//---------------------------------------------------------------------------
+
+//  Greenplum Database
+//  Copyright (C) 2018 Pivotal Inc.
+//
+//  @filename:
+//      CXformEagerAgg.h
+//
+//  @doc:
+//      Eagerly push aggregates below join when there is no primary/foreign keys
+//---------------------------------------------------------------------------
+#ifndef GPOPT_CXformEagerAgg_H
+#define GPOPT_CXformEagerAgg_H
+
+#include "gpos/base.h"
+#include "gpopt/xforms/CXformExploration.h"
+
+namespace gpopt
+{
+	using namespace gpos;
+
+	//---------------------------------------------------------------------------
+	//  @class:
+	//      CXformEagerAgg
+	//
+	//  @doc:
+	//      Eagerly push aggregates below join when there is no primary/foreign keys
+	//
+	//---------------------------------------------------------------------------
+	class CXformEagerAgg : public CXformExploration
+	{
+
+		private:
+
+			// private copy ctor
+			CXformEagerAgg(const CXformEagerAgg &);
+
+
+		public:
+
+			// ctor
+			explicit
+			CXformEagerAgg(IMemoryPool *mp);
+
+			// ctor
+			explicit
+			CXformEagerAgg(CExpression *pexprPattern);
+
+			// dtor
+			virtual
+			~CXformEagerAgg()
+			{}
+
+			// ident accessors
+			virtual
+			EXformId Exfid() const
+			{
+				return ExfEagerAgg;
+			}
+
+			virtual
+			const CHAR *SzId() const
+			{
+				return "CXformEagerAgg";
+			}
+
+			// compatibility function for eager aggregation
+			virtual
+			BOOL FCompatible(CXform::EXformId exfid)
+			{
+				return (CXform::ExfEagerAgg != exfid) &&
+							(CXform::ExfSplitGbAgg != exfid);
+			}
+
+			// compute xform promise for a given expression handle
+			virtual
+			EXformPromise Exfp(CExpressionHandle &exprhdl) const;
+
+			// actual transform
+			void Transform(CXformContext *pxfctxt, CXformResult *pxfres, CExpression *pexpr) const;
+
+			// check if Transform can be applied
+			BOOL FApplicable(CExpression *pexpr, CColRefSet *push_down_gb_crs) const;
+
+        	// is this agg supported for push down?
+        	BOOL IsAggSupported (CExpression *scalar_agg_func_expr) const;
+
+			void PopulateLowerUpperProjectList
+			(
+			 IMemoryPool *mp,               // memory pool
+			 CExpression *orig_proj_list,   // project list of the original global aggregate
+			 CExpression **lower_proj_list, // project list of the new lower aggregate
+			 CExpression **upper_proj_list  // project list of the new upper aggregate
+			) const;
+
+			// return true if xform should be applied only once
+			virtual
+			BOOL IsApplyOnce(){
+				return true;
+			 };
+	}; // class CXformEagerAgg
+
+}
+
+#endif // !GPOPT_CXformEagerAgg_H
+
+// EOF
