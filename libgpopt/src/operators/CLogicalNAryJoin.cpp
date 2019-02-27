7145979bc3080b420777c66d1ad6d31169d15f59
Bhuvnesh Chaudhary <bhuvnesh2703@gmail.com>
2019-02-26 17:26:35 -0800

    test

--- libgpopt/src/operators/CLogicalNAryJoin.cpp
+++ libgpopt/src/operators/CLogicalNAryJoin.cpp
@@ -74,11 +74,11 @@ CLogicalNAryJoin::PxfsCandidates
 {
 	CXformSet *xform_set = GPOS_NEW(mp) CXformSet(mp);
 	
-//	(void) xform_set->ExchangeSet(CXform::ExfSubqNAryJoin2Apply);
+	(void) xform_set->ExchangeSet(CXform::ExfSubqNAryJoin2Apply);
 //	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoin);
 	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoinMinCard);
-	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoinDP);
-	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoinGreedy);
+//	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoinDP);
+//	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoinGreedy);
 	(void) xform_set->ExchangeSet(CXform::ExfExpandNAryJoinHybridGreedyIk);
 
 	return xform_set;
