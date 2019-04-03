272acc564efbf9c74a4f27dd4eff84febb77bdfd
Hans Zeller <hzeller@pivotal.io>
2019-04-03 14:10:33 -0700

    Fix reorder

--- libgpopt/src/xforms/CXformFactory.cpp
+++ libgpopt/src/xforms/CXformFactory.cpp
@@ -290,8 +290,8 @@ CXformFactory::Instantiate()
 	Add(GPOS_NEW(m_mp) CXformLeftOuterJoinWithInnerSelect2IndexGetApply(m_mp));
 	Add(GPOS_NEW(m_mp) CXformExpandNAryJoinGreedy(m_mp));
 	Add(GPOS_NEW(m_mp) CXformEagerAgg(m_mp));
-	Add(GPOS_NEW(m_mp) CXformExpandNAryJoinDynamicProg(m_mp));
 	Add(GPOS_NEW(m_mp) CXformExpandNAryJoinDynamicProgramming(m_mp));
+	Add(GPOS_NEW(m_mp) CXformExpandNAryJoinDynamicProg(m_mp));
 
 	GPOS_ASSERT(NULL != m_rgpxf[CXform::ExfSentinel - 1] &&
 				"Not all xforms have been instantiated");
